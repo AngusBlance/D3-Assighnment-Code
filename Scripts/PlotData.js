d5edda6a38d6afe1fbac2df6082322c7ced7fd3b
AngusBlance <contact.angusgb@gmail.com>
2023-11-07 20:31:37 +0000

    create the map with dots

--- Scripts/PlotData.js
+++ Scripts/PlotData.js
@@ -1,26 +1,86 @@
-const Height = 2000
-const width = 1500
+//set up constants for the width and hight of csv
+const width = 1000;
+const height = 600;
 
-// create svg within body of html
-var svg = d3.select("body").append("svg").attr("width", Width).attr(
-    "Height", Height);
-// create circles from d3
-var circles = svg.selectAll("circle").data(dataset).enter()
-		.append("circle");
+//set up constants for how small we want our bubbles as well as our scale factor 
+const size = 15000;
+const x = 1;
+const TheScale = 1700;
+const center = [6, 56];
+
+var UKGeoJson = "https://raw.githubusercontent.com/martinjc/UK-GeoJSON/master/json/administrative/gb/lad.json";
+var NIGeoJson = "https://raw.githubusercontent.com/martinjc/UK-GeoJSON/master/json/administrative/ni/lgd.json";
+
+// Create an SVG container
+var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+// Create a projection (here, using Mercator projection)
+var projection = d3.geoMercator()
+  .center(center)
+  .scale(TheScale)
+  .translate([width / 2, height / 2]);
+
+// Create a GeoPath generator and set the projection
+var path = d3.geoPath().projection(projection);
 
 
-function d3Draw(dataset){
-    circles.attr(cx, function(dataset){var coords = projection([dataset.lng,dataset.lat]); return coords[0];})
-    .attr("cy", function(dataset){var coords = projection([dataset.lng,dataset.lat]); return coords[1];})
-    .attr("r", function(dataset){var Psize = 1+2*Math.sqrt(dataset.Population/10000); return Psize;})
+/*
+function reads the data from the json file and turns it in to bubbles
+*/
+
+function d3Draw(dataset) {
+
+
+  //load our json file
+  d3.json('http://34.38.72.236/Circles/Towns/50').then(data => {
+    //create circles
+    svg.selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      //turn the lattitude and longitude into c,y coordinates with projection function
+      .attr('cx', d => x * (projection([d.lng, d.lat])[0]))
+      .attr('cy', d => x * (projection([d.lng, d.lat])[1]))
+      //make the radius proporional to the population size of each place
+      //use the size constant to make sure they are not too big
+      .attr('r', function (d) {
+        return d.Population / size
+      })
+      .style('fill', '#c23616');
+      
+  }).catch(error => console.error(error));
+}
+
+function drawmap(link) {
+  // Load GeoJSON data
+  d3.json(link).then(function (data) {
+    // Draw features on the map
+    svg.selectAll("path")
+      .data(data.features)
+      .enter().append("path")
+      .attr("d", path)
+      .style("fill", "#44bd32")
+
+  });
+
+
+
+
 }
 
 
-//reads data and then calls d3Draw function with the data inputed
-function    ReadData(){
-    var datalink = "http://34.38.72.236/Circles/Towns/50"
-    d3.json("datalink", function(data) {
-		d3Draw(data);
-	});
-}
-window.onload = ReadData;
+
+
+window.addEventListener("load", function () {
+
+  drawmap(UKGeoJson);
+  drawmap(NIGeoJson);
+
+  setTimeout(function () {
+    d3Draw();
+    //your code to be executed after 1 second
+  }, 2000);
+
+});
